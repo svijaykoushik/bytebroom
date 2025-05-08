27be688818d7b04876b607abfc14c59cf126cafb
Vijay Koushik, S. <svijaykoushik@hotmail.com>
2025-05-08 09:26:13 +0530

    30 cli crashes on nodejs v20 after successful install (#31)
    * fix: Check min version requirement
    
    Checks the minimum version requirement from the engine directive
    in package.json
    
    Previously the package crashed in unsupported node versions
    
    * ci: Add workflows publish and release
    
    Updates build workflow to be reusable
    
    created pr workflow, publish workflow and release workflows
    
    * chore: Update readme
    
    Updates readme to show description first and then badges
    
    Previously the badges code was shown as description in npm registry
    
    * ci: Fix build script typo
    
    Fixes the typo in the build script

--- packages/cli/bin/index.js
+++ packages/cli/bin/index.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
-require("../dist/index.js")
+const fs = require('fs');
+const path = require('path');
+const semver = require('semver'); // For more robust version comparison
+
+// Function to read package.json
+function readPackageJson() {
+    try {
+        const packageJsonPath = path.join(__dirname, '..', 'package.json');
+        const rawData = fs.readFileSync(packageJsonPath, 'utf8');
+        return JSON.parse(rawData);
+    } catch (error) {
+        console.error('Error reading package.json:', error);
+        process.exit(1);
+    }
+}
+
+const packageJson = readPackageJson();
+const requiredNodeVersion = packageJson.engines && packageJson.engines.node;
+const currentVersion = process.version;
+
+if (requiredNodeVersion && !semver.satisfies(currentVersion, requiredNodeVersion)) {
+    console.error(
+        `\x1b[31mError: @bytebroom/cli requires Node.js version ${requiredNodeVersion}. ` +
+        `You are using ${currentVersion}.\x1b[0m`
+    );
+    process.exit(1);
+}
+
+require("../dist/index.js");
